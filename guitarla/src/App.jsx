d92b37367152b01089adbb41fe6a37dfed40a3db
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-19 22:39:35 -0600

    useCart hook created

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header" // Importar el componente
 import { db } from "./data/db"
+import { useCart } from "./hooks/useCart"
 
 function App() {
 
+    useCart()
+    
     const initialCart = () => {
         const localStorageCart = localStorage.getItem('cart')
         return localStorageCart ? JSON.parse(localStorageCart) : []
